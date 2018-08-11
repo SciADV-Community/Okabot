f1b186a0f8071c2337931842f11fda81dd8a5c2b
Zorpos <37426682+Zorpos@users.noreply.github.com>
2018-08-11 18:30:50 -0400

    Fixed duplicate logging

--- logger.py
+++ logger.py
@@ -1,10 +1,6 @@
-from systemd.journal import JournaldLogHandler
 import logging
 
 logger = logging.getLogger(__name__)
-journal_handler = JournaldLogHandler()
-journal_handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
-logger.addHandler(journal_handler)
 logger.setLevel(logging.INFO)
 
 logging.debug("Logging enabled")
